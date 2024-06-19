e24e916f4e0713feb68f6b9cf53c58ca9b915c5d
Mawuli87 <yawomessie@gmail.com>
2024-06-19 04:38:34 +0000

    Login UI and Password Validation

---
+++ app/(routes)/login/index.tsx
@@ -0,0 +1,7 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import LoginScreen from "@/screens/auth/login/login.screen";
+
+export default function Login() {
+  return <LoginScreen />;
+}
