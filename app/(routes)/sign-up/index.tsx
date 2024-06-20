52a37484e0559a097fdb47afb55349542ce88653
Mawuli87 <yawomessie@gmail.com>
2024-06-20 15:20:13 +0000

    Ui Completed

---
+++ app/(routes)/sign-up/index.tsx
@@ -0,0 +1,7 @@
+import { View, Text } from "react-native";
+import React from "react";
+import SignupScreen from "@/screens/auth/sign-up/signup.screen";
+
+export default function SignUp() {
+  return <SignupScreen />;
+}
